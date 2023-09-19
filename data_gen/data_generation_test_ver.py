848b1a7b2bad1642d859523cadd70d95809fae52
aimh95 <aimh95@naver.com>
2023-09-19 09:13:40 +0900

    테스트용 데이터 제네레이터 코드 수정 1. 녹화 시작, 종료 시점 설정 기능 추가 2. 특정 프레임 이미지 추출 기능 추가 3. 자막 확률 분포를 화면 좌상단에 출력하는 기능 추가    _subtitle_prob_burn_in(self, image, subtitle_prob)

---
+++ data_gen/data_generation_test_ver.py
@@ -0,0 +1,139 @@
+import random
+from PIL import Image, ImageFont, ImageDraw
+import tensorflow as tf
+import cv2
+import os
+import glob
+from models.real_u_net import U_Net
+import string
+from tensorflow.python.data.experimental import AUTOTUNE
+import numpy as np
+import tensorflow_datasets as tfds
+import matplotlib.pyplot as plt
+import math
+from utils.utilities import unsqueeze
+
+class ReadVideoAsData(tf.keras.utils.Sequence):
+    def __init__(self, dataset_path, output_path, model_weight_path, model, padding = 0):
+        super(ReadVideoAsData, self).__init__()
+        # self.dataset_path = dataset_path
+        self.data_dir = dataset_path
+        self.output_dir = output_path
+        self.weight_path = model_weight_path
+
+        self.model = model
+        self.padding = padding
+
+        self.video_capture, self.video_spf = self._read_vid_file()
+        self.video_writer = self._video_writer_initializer()
+
+    def _run(self, start_time=None, end_time=None, frame=None):
+        self._model_weight_loader()
+        success = True
+        time_stamp = 0
+        frame_num = 0
+
+        while success:
+            frame_num += 1
+            success, image = self.video_capture.read()
+            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+            time_stamp = frame_num * self.video_spf
+
+            if start_time == None and end_time == None:
+                data_input = self._crop_roi(image, padding = self.padding)
+                mapping_result_of_roi, subtitle_prob = self._get_subtitle_mapping_result(data_input)
+                mapping_result, mapping_result_on_frame = self._subtitle_mapping_full_size_recon(image, mapping_result_of_roi)
+
+                # self._plot_on_screen(mapping_result, "mapping_result")
+                # self._plot_on_screen(mapping_result_on_frame/255., "mapping_result_on_frame")
+                self._save_in_dir(mapping_result_on_frame)
+            elif start_time <= time_stamp and end_time >= time_stamp:
+                data_input = self._crop_roi(image, padding=self.padding)
+                mapping_result_of_roi, subtitle_prob = self._get_subtitle_mapping_result(data_input)
+                mapping_result, mapping_result_on_frame = self._subtitle_mapping_full_size_recon(image, mapping_result_of_roi)
+                # self._plot_on_screen(mapping_result, "mapping_result")
+                mapping_result_on_frame = self._subtitle_prob_burn_in(mapping_result_on_frame, subtitle_prob)
+                # self._plot_on_screen(mapping_result_on_frame / 255., "mapping_result_on_frame")
+
+                self._save_in_dir(mapping_result_on_frame)
+
+            if frame!=None and frame_num==frame:
+                data_input = self._crop_roi(image, padding=self.padding)
+                mapping_result_of_roi, subtitle_prob = self._get_subtitle_mapping_result(data_input)
+                mapping_result, mapping_result_on_frame = self._subtitle_mapping_full_size_recon(image, mapping_result_of_roi)
+                self._capture_spcf_frame(mapping_result_on_frame, frame_num)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        return
+
+
+    def _capture_spcf_frame(self, image, frame):
+        output_dir = os.path.join(self.output_dir, self.weight_path.split("/")[1], str(frame))
+        cv2.imwrite(output_dir, image)
+        return
+
+    def _model_weight_loader(self):
+        self.model.load_weights(self.weight_path)
+
+    def _crop_roi(self, input_frame, padding = 0):
+        height, width, ch = input_frame.shape
+        cropped_img = input_frame[height * 2 // 3:, padding:width - padding, :]
+        return cropped_img
+
+    def _get_subtitle_mapping_result(self, data_input):
+        data_input = unsqueeze(data_input/255.)
+        output = self.model.predict(data_input, verbose = 2)
+        return output, np.sum(output)
+
+    def _subtitle_mapping_full_size_recon(self, input_frame, roi_mapping):
+        original_height, original_width = self.frame_size
+        cropped_height_start_y = original_height * 2 // 3
+        recon_mapping = np.zeros(shape=(original_height, original_width, 1))
+        recon_mapping[cropped_height_start_y:, self.padding:original_width - self.padding, :] = roi_mapping
+        input_frame[:, :, 0:1] += (recon_mapping*255).astype(np.uint8)
+        mapping_frame = np.clip(input_frame, 0, 255)
+        # plt.imshow(mapping_frame)
+        return recon_mapping, mapping_frame
+
+    def _plot_on_screen(self, result, winname):
+        cv2.imshow(winname, result)
+
+    def _subtitle_prob_burn_in(self, image, subtitle_prob):
+        image_pil = tf.keras.utils.array_to_img(image)
+        draw = ImageDraw.Draw(image_pil)
+        font = ImageFont.truetype(font="./utils/movie_fonts/arial_bold.ttf", size=32)
+        draw.text((10, 10), str(subtitle_prob), fill="red", font=font)
+        return tf.keras.utils.img_to_array(image_pil).astype(np.uint8)
+
+    def _video_writer_initializer(self):
+        fourcc = cv2.VideoWriter_fourcc(*'DIVX')
+        output_dir = os.path.join(self.output_dir, self.weight_path.split("/")[1])
+        if not os.path.isdir(output_dir):
+            os.mkdir(output_dir)
+        file_name = self.weight_path.split(".")[0][-4:]+self.data_dir.split("/")[-1]
+        mapping_video_writer = cv2.VideoWriter(os.path.join(output_dir, file_name), fourcc, 1/self.video_spf, (self.frame_size[1], self.frame_size[0]))
+        # concat_output = cv2.VideoWriter('concat_output.mp4', fourcc, 23.976, (1280, 694 * 2))
+        return mapping_video_writer
+
+    def _save_in_dir(self, result):
+        # print(self.video_writer.isOpened())
+        plt.imshow(result)
+        result = cv2.cvtColor(result, cv2.COLOR_RGB2BGR)
+        self.video_writer.write(result)
+
+
+    def _read_vid_file(self):
+        if os.path.isfile(self.data_dir):
+            vidcap = cv2.VideoCapture(self.data_dir)
+            # vidcap.set(cv2.CAP_PROP_FRAME_WIDTH, 360)
+            # vidcap.set(cv2.CAP_PROP_FRAME_HEIGHT,240)
+            vid_spf = 1 / vidcap.get(cv2.CAP_PROP_FPS)
+            self.frame_size = (int(vidcap.get(cv2.CAP_PROP_FRAME_HEIGHT)), int(vidcap.get(cv2.CAP_PROP_FRAME_WIDTH)))
+            return vidcap, vid_spf
+        else:
+            print("there is no file in this directory")
+            return
+
+
+# dataloader = CustomDataLoader("../datasets/train_dataset/indoor")
+# x, y = dataloader.data_dir()
